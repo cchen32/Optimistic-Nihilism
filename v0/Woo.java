1cb1555cea9fed95730b3b569ef9e5cbc76189c2
Corina Chen <cchen32@Corinas-MacBook-Pro.local>
2022-05-26 19:55:57 -0400

    greetings

--- v0/Woo.java
+++ v0/Woo.java
@@ -1,3 +1,9 @@
 public class Woo {
   // Launch pad
+
+  public static void main(String[] args) {
+    // Greet the travellers/explorers
+    System.out.println("\nWelcome, Explorers, to the Mysterious Cave of the Unknown.\n");
+    System.out.println("Do you wish to procede?\n");
+  }
 }
