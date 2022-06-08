a7d27f5efbe877246903860b31db1d45301da164
cchen32 <cchen32@stuy.edu>
2022-06-08 12:34:03 -0400

    colorize and more bugs

---
+++ v7/Monster.java
@@ -0,0 +1,6 @@
+public class Monster extends Character{
+  public Monster(){
+    _health = 1200;
+    _attack = 350;
+  }
+}
---
+++ v7/Woo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Woo {
+  // Launch pad
+
+  public static void main(String[] args) {
+    Game rpg = new Game();
+
+    // Greet the travellers/explorers
+    // System.out.println("\nWelcome, Explorer, to the Mysterious Cave of the Unknown.\n");
+    String welcome = "\nWelcome, Explorer, to the Mysterious Cave of the Unknown.\n";
+    Text.colorize(welcome, 30);
+
+    // Asks for the player's name
+    System.out.println("What is your name, chosen hero?\n");
+    Scanner in = new Scanner(System.in);
+    String response = in.nextLine();
+    System.out.println("\n" + rpg.heroName(response) + "\n");
+
+    // Asks if player wants to play. Valid responses are "yes" and "no"
+    System.out.println("Do you wish to procede?\n");
+    response = in.nextLine();
+    rpg.starting(response);
+  }
+}
